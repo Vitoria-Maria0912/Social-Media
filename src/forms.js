e56dbce3cad37a688b2bcf2750364c5b8879a7e4
Vitoria-Maria0912 <vitoria.nascimento@ccc.ufcg.edu.br>
2024-05-23 13:41:55 -0300

    wip: Adicionando imagem ao perfil e verificando se o campo está preenchido.

--- src/forms.js
+++ src/forms.js
@@ -1,8 +1,14 @@
+/**
+ * Shows the profile (if the form wasn't answered the default)
+ */
 const profile = document.getElementById("profile");
 profile.addEventListener("click", () => {
     window.open("profile.html", "_blank");
 });
 
+/**
+ * Form who asks if the user want to create a profile
+ */
 const createProfile = document.getElementById("createProfile");
 createProfile.addEventListener("submit", function(event) {
 
@@ -14,10 +20,13 @@ createProfile.addEventListener("submit", function(event) {
     (yesOption.checked) ? (document.getElementById("sendInformation").style.display = "block",
                            createProfile.style.display = "none") :
     (noOption.checked) ? (document.getElementById("profile").style.display = "block",
-                          document.getElementById("profile").textContent = "👉🏽 Click here to see an exemplo 👈🏽")
+                          document.getElementById("profile").textContent = "👉🏽 Click here to see an example 👈🏽")
     : alert("Select an option!");
 });
 
+/**
+ * Send the information (name, nickname and biografy) to create a new profile
+ */
 const sendInformation = document.getElementById("sendInformation");
 sendInformation.addEventListener("submit", function(event) {
 
@@ -27,18 +36,29 @@ sendInformation.addEventListener("submit", function(event) {
     const nickname = document.getElementById("nickname");
     const biografy = document.getElementById("biografy");
 
-    (validName(name) && validNickname(nickname)) ?
-    (document.getElementById("addAphoto").style.display = "block", createProfile.style.display = "none"): null;
+    (validName(name) && validNickname(nickname)) ? (document.getElementById("addAphoto").style.display = "block",
+                                                    document.getElementById("profile").style.display = "block", 
+                                                    document.getElementById("profile").textContent = "👉🏽 Click here to see a preview 👈🏽"): null;
 });
 
+/**
+ * Verified if the attribute name isn't empty, composed by numbers or special caracters.
+ * @param name 
+ * @returns if the name is valid or not
+ */
 function validName(HTMLElement) {
     const regex = new RegExp('^[A-Za-zÀ-ÖØ-öø-ÿ\\s]+$');
     let isValid = false;
     (HTMLElement.value === "") ? alert("Name is required!") :
     (regex.test(HTMLElement.value) === false) ? alert("Name cannot contain numbers or special caracters!"): isValid = true;
     return isValid;
-}
+};
 
+/**
+ * Verified if the attribute nickname isn't empty, composed only by numbers or special caracters.
+ * @param nickname 
+ * @returns if the nickname is valid or not
+ */
 function validNickname(HTMLElement) {
     const regex = new RegExp('^[A-Za-z0-9@_.\\s]+$');
     const onlyNumbers = new RegExp('^[0-9@_.\\s]+$');
@@ -46,5 +66,42 @@ function validNickname(HTMLElement) {
     (HTMLElement.value === "") ? alert("Nickname is required!") :
     (onlyNumbers.test(HTMLElement.value) === true) ? alert("Nickname cannot contain only numbers! (or @_.)") :
     (regex.test(HTMLElement.value) === false) ? alert("Nickname cannot contain special caracters! (besides @_.)"): isValid = true;
+    return isValid;
+}
+
+/**
+ * Form who asks if the user want to add a photo to profile
+ */
+const addAphoto = document.getElementById("addAphoto");
+addAphoto.addEventListener("submit", function(event) {
+
+    event.preventDefault();
+    
+    const yesOption = document.getElementById("yes");
+    const noOption = document.getElementById("no");
+
+    (yesOption.checked) ? (document.getElementById("putTheImage").style.display = "block") :
+    (noOption.checked) ? null
+    : alert("Select an option!");
+});
+
+const putTheImage = document.getElementById("putTheImage");
+putTheImage.addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    const linkImage = document.getElementById("image");
+
+    (verifiedLikability(linkImage)) ? document.getElementById("congratulations").style.display = "block": null;
+});
+
+/**
+ * Verified if the link to the picture is valid.
+ * @param link 
+ * @returns if the link is valid or not
+ */
+function verifiedLikability(link) {
+    let isValid = false;
+    (link.value === "") ? alert("Link is required!") : isValid = true;
     return isValid;
 }
